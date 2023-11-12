3dc445c8b7ff158a505eb6c3d21c7afd1bbb8bcf
AhmadShamouka <shamoukaahmad@gmail.com>
2023-11-13 01:01:07 +0200

    creating function addeventlistener for each button

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const yellowDiv = document.getElementById('yellow');
 const audioBlue = new Audio('./sounds/red.mp3');
 const blueDiv = document.getElementById('blue');
 
+let buttonArray=[]
 let level=1;
 let autoArray=[];
 
@@ -18,30 +19,101 @@ const audioArray = [
   { id: 2, audio: audioBlue,name:blueDiv},
   { id: 3, audio:  audioRed,name:yellowDiv},
   { id: 4, audio:  audioYellow,name:redDiv}
+
 ];
 
 function getRandomAudio() {
   const randomIndex = Math.floor(Math.random() * audioArray.length);
   return audioArray[randomIndex];
 }
-
 function generateRandom() {
-  for(i=0;i<level;i++){
-      const randomAudio = getRandomAudio();
-      randomAudio.name.classList.add('pressed');
-      randomAudio.audio.play();
+  
+      const randomAudio = getRandomAudio()
       autoArray.push(randomAudio.id);
-      console.log(autoArray)
-      setTimeout(() => {
-        randomAudio.name.classList.remove('pressed');
-      }, 1000);
+      for(i=0;i<autoArray.length;i++){
+        setTimeout(()=>{
+          randomAudio.name.classList.add('pressed');
+          randomAudio.audio.play();
+          setTimeout(() => {
+            randomAudio.name.classList.remove('pressed');
+          }, 1000); 
+        },2000)  ; 
 }
-level++;
+console.log(autoArray)
 }
 
+greenDiv.addEventListener('click', function() {
 
+  greenDiv.classList.add('pressed');
+  audioGreen.play();
+  setTimeout(() => {
+    greenDiv.classList.remove('pressed');
+  }, 1000);
+  buttonArray.push(1)
+  console.log(buttonArray)
+  compareTo()
+});
+
+
+redDiv.addEventListener('click', function() {
+  redDiv.classList.add('pressed');
+  audioRed.play();
+  setTimeout(() => {
+    redDiv.classList.remove('pressed');
+  }, 1000);
+  buttonArray.push(4)
+  console.log(buttonArray)
+  compareTo()
+});
+
+yellowDiv.addEventListener('click', function() {
+  yellowDiv.classList.add('pressed');
+  audioYellow.play();
+  setTimeout(() => {
+    yellowDiv.classList.remove('pressed');
+  }, 1000);
+  buttonArray.push(3)
+  console.log(buttonArray)
+  compareTo()
+});
+
+blueDiv.addEventListener('click', function() {
+  blueDiv.classList.add('pressed');
+  audioBlue.play();
+  setTimeout(() => {
+    blueDiv.classList.remove('pressed');
+  }, 1000);
+  buttonArray.push(2)
+  console.log(buttonArray)
+  compareTo()
+});
+  
+function compareTo(){
+  if(buttonArray.length === autoArray.length){
+    for (let i = 0; i < buttonArray.length; i++) {
+        if (buttonArray[i] !== autoArray[i]) {
+            autoArray=[]
+            buttonArray=[]
+            return;
+        }
+    }
+    generateRandom()
+    buttonArray=[]
+  }
+  
+  else if (buttonArray.length > autoArray.length) {
+    document.body.style.backgroundColor ='red';
+
+    
+    autoArray = [];
+    buttonArray = [];
+    setTimeout(() => {
+      document.body.style.backgroundColor =''; 
+    }, 1000); 
+  }
+  
+}
 
 document.addEventListener('keydown', function(event) {
   generateRandom();
-
 });
